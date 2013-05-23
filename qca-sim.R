7058216e3181ad6f3da1e981d4bb5cc75610518a
jwbowers <jake@jakebowers.org>
2013-05-23 00:04:50 -0500

    next

--- qca-sim.R
+++ qca-sim.R
@@ -1,5 +1,10 @@
 ## If any of the following packages are not installed, install them.
-
+neededpkgs<-c("QCA","glmnet","SIS","compiler","parallel","data.table")
+localpkgs<-installed.packages()
+to.install<-neededpkgs[!(neededpkgs %in% localpkgs[,"Package"])]
+if(length(to.install)>0){
+  install.packages(to.install,dependencies=TRUE)
+}
 
 ## Now load the packages
 library(QCA)
@@ -12,10 +17,10 @@ numcores<-detectCores()
 
 makedatamatrix<-function(ntotalvars,N){
   ## Here we allow each column of X to have different numbers of 1s versus 0s.
-  ## data.table(replicate(ntotalvars,sample(c(1,0),size=N,replace=TRUE)))
+  data.table(replicate(ntotalvars,sample(c(1,0),size=N,replace=TRUE)))
 
   ## Here we require half of each X to be 1 versus 0.
-  data.table(replicate(ntotalvars,sample(rep(c(1,0),N/2))))
+  ## data.table(replicate(ntotalvars,sample(rep(c(1,0),N/2))))
 }
 
 makeoutcome<-function(X,expr){
@@ -43,8 +48,8 @@ fitfn<-function(y,X,DAT){
   ## Adaptive Lasso
   ### Choose lambda by minimizing misclassification
   theridge.cv <- try( cv.glmnet(X, yF, alpha=0, type.measure="class",
-                                family="binomial", standardize=TRUE,
-                                nfolds=min(round(nrow(X)/2), 10), grouped=FALSE))
+				family="binomial", standardize=TRUE,
+				nfolds=min(round(nrow(X)/2), 10), grouped=FALSE))
 
   if(inherits(theridge.cv,'try-error')){
     lassofound<-NA
@@ -57,12 +62,12 @@ fitfn<-function(y,X,DAT){
     adpen<-(1/pmax(abs(bhat),.Machine$double.eps))
 
     thelasso.cv<-try( cv.glmnet(X,yF,alpha=1,type.measure="class",
-                                family="binomial",
-                                exclude=which(bhat==0),
-                                penalty.factor=adpen,
-                                standardize=TRUE,
-                                nfolds=min(round(nrow(X)/2),10),
-                                grouped=FALSE) )
+				family="binomial",
+				exclude=which(bhat==0),
+				penalty.factor=adpen,
+				standardize=TRUE,
+				nfolds=min(round(nrow(X)/2),10),
+				grouped=FALSE) )
 
     if(inherits(thelasso.cv,'try-error')){
       lassofound<-NA
@@ -73,91 +78,108 @@ fitfn<-function(y,X,DAT){
     }
   }
 
-    ## QCA
-    theqca<-try(eqmcc(DAT,outcome="Y"))
-    ### Did QCA return the truth and only the truth?
-    if(inherits(theqca,'try-error')){
-      qcafound<-NA
-    } else {
-      qcafound<-all(theqca$PIs %in% truthparts)
-    }
+  ## QCA
+  theqca<-try(eqmcc(DAT,outcome="Y"))
+  ### Did QCA return the truth and only the truth?
+  if(inherits(theqca,'try-error')){
+    qcafound<-NA
+  } else {
+    qcafound<-all(theqca$PIs %in% truthparts)
+  }
 
-    ## SIS/ISIS
-    thesis<-try( SIS(data=list(x=X,y=y),
-                family=binomial(),
-                folds=min(round(nrow(X)/2),10),
-                maxloop=10,
-                detail=FALSE) )
-    if(inherits(thesis,'try-error')){
-      sisfound<-NA 
-    } else {
+  ## SIS/ISIS
+  thesis<-try( SIS(data=list(x=X,y=y),
+		   family=binomial(),
+		   folds=min(round(nrow(X)/2),10),
+		   maxloop=10) )
+  if(inherits(thesis,'try-error')){
+    sisfound<-NA 
+  } else {
     sisfound <- all( gsub("*",":",truthparts,fixed=TRUE) %in% colnames(X)[thesis$ISISind])
-    }
-
-    return(c(qcafound=qcafound,
-             lassofound=lassofound,
-             sisfound=sisfound))
-  } 
-
-
-  myfn.maker<-function(ntotalvars,N,thetruth){
-    force(ntotalvars);force(N);force(thetruth)
-    function(){
-      thedata<-makedatamatrix(ntotalvars,N)
-      theX<-makemodelmatrix(thedata,4) ## fourway interactions
-      ## theX<-unique(theX,MARGIN=2) ## delete identical columns
-      theY<-makeoutcome(thedata,thetruth)
-      ##while(sum(theY)==1 | any(colSums(theX) %in% c(0,N))){
-      ##  ## don't allow constant Xs
-      ##  thedata<-makedatamatrix(ntotalvars,N)
-      ##  theX<-makemodelmatrix(thedata,4) ## fourway interactions
-      ##  ## theX<-unique(theX,MARGIN=2) ## delete identical columns
-      ##  theY<-makeoutcome(thedata,thetruth)
-      ##}
-      names(theY)<-row.names(theX)
-      ## Some functions fail if we substitute data.table class objects for
-      ## data.frame objects.
-      thedf<-data.frame(thedata)
-      thedf$Y<-theY
-      fitfn(y=theY,X=theX,DAT=thedf)
-    }
   }
 
 
+  ## KRLS
+  ##   thekrls<-krls(thedata,theY,derivative=TRUE)
+  ##   derivmat<-thekrls$derivatives
+  ##   colnames(derivmat)<-colnames(thekrls$X)
+  ## 
+  ##   krlsfound.fn<-function(one,two,X=thekrls$X,dmat=derivmat){
+  ##     thelm<-lm(X[,one]~dmat[,two])
+  ##     thesum<-summary(thelm)
+  ##     pf(thesum$fstatistic[1],thesum$fstatistic[2],thesum$fstatistic[3],
+  ##        lower.tail=FALSE)<=.05
+  ##   }
+  ## 
+  ##   krlsfound<-all(krlsfound.fn("V1","V2"),
+  ## 		 krlsfound.fn("V4","V5"))
 
-  ## Start doing simulations
-  ### Setup data. Some routines want matrices others want data.frames
-  nsims<-100*numcores
 
-  ## Easy sim: All variables in truth, p<n
-  thetruth<-"V1*V2*V3 | V4*V5"
-  ntotalvars<-5
-  N<-30
 
-  myfn<-myfn.maker(ntotalvars,N,thetruth)
-  cmp.myfn<-cmpfun(myfn,options=list(optimize=3))
+  return(c(qcafound=qcafound,
+	   lassofound=lassofound,
+	   sisfound=sisfound))
+} 
 
-  set.seed(12345) ## for replicability
-  ## mysim1<-replicate(nsims,cmp.myfn())
-  mysim1<-mclapply(1:nsims,function(i){ cmp.myfn() },mc.cores=numcores)
-  mysim1.arr<-simplify2array(mysim1)
-  apply(mysim1.arr,1,mean,na.rm=TRUE) ## proportion of the time that qca and isis/scad and lasso identified the right terms
-  apply(mysim1.arr,1,function(x){mean(is.na(x)) }) ## what proportion NAs in each method?
 
-  save(mysim1,file="mysim1.rda")
+myfn.maker<-function(ntotalvars,N,thetruth){
+  force(ntotalvars);force(N);force(thetruth)
+  function(){
+    thedata<-makedatamatrix(ntotalvars,N)
+    theX<-makemodelmatrix(thedata,4) ## fourway interactions
+    ## theX<-unique(theX,MARGIN=2) ## delete identical columns
+    theY<-makeoutcome(thedata,thetruth)
+    while(sum(theY)==1 | any(colSums(theX) %in% c(0,N))){
+    ##  ## don't allow Y with only 1 positive obs 
+      thedata<-makedatamatrix(ntotalvars,N)
+      theX<-makemodelmatrix(thedata,4) ## fourway interactions
+    ##  ## theX<-unique(theX,MARGIN=2) ## delete identical columns
+      theY<-makeoutcome(thedata,thetruth)
+    }
+    names(theY)<-row.names(theX)
+    ## Some functions fail if we substitute data.table class objects for
+    ## data.frame objects.
+    thedf<-data.frame(thedata)
+    thedf$Y<-theY
+    fitfn(y=theY,X=theX,DAT=thedf)
+  }
+}
 
-  ## Very hard sim: most variables not in truth, p>n
-  ntotalvars<-15
 
-  myfn<-myfn.maker(ntotalvars,N,thetruth)
-  cmp.myfn<-cmpfun(myfn,options=list(optimize=3))
 
-  set.seed(12345) ## for replicability
-  ## mysim2<-replicate(nsims,cmp.myfn())
-  mysim2<-mclapply(1:nsims,function(i){ cmp.myfn() },mc.cores=numcores)
-  mysim2.arr<-simplify2array(mysim2)
-  apply(mysim2.arr,1,mean,na.rm=TRUE) ## proportion of the time that qca and isis/scad and lasso identified the right terms
-  apply(mysim2.arr,1,function(x){mean(is.na(x)) }) ## what proportion NAs in each method?
+## Start doing simulations
+### Setup data. Some routines want matrices others want data.frames
+nsims<-100*numcores
 
-  save(mysim2,file="mysim2.rda")
+## Easy sim: All variables in truth, p<n
+thetruth<-"V1*V2*V3 | V4*V5"
+ntotalvars<-5
+N<-40
+
+myfn<-myfn.maker(ntotalvars,N,thetruth)
+cmp.myfn<-cmpfun(myfn,options=list(optimize=3))
+
+set.seed(12345) ## for replicability
+## mysim1<-replicate(nsims,cmp.myfn())
+mysim1<-mclapply(1:nsims,function(i){ cmp.myfn() },mc.cores=numcores)
+mysim1.arr<-simplify2array(mysim1)
+apply(mysim1.arr,1,mean,na.rm=TRUE) ## proportion of the time that qca and isis/scad and lasso identified the right terms
+apply(mysim1.arr,1,function(x){mean(is.na(x)) }) ## what proportion NAs in each method?
+
+save(mysim1,file="mysim1.rda")
+
+## Very hard sim: most variables not in truth, p>n
+ntotalvars<-15
+
+myfn<-myfn.maker(ntotalvars,N,thetruth)
+cmp.myfn<-cmpfun(myfn,options=list(optimize=3))
+
+set.seed(12345) ## for replicability
+## mysim2<-replicate(nsims,cmp.myfn())
+mysim2<-mclapply(1:nsims,function(i){ cmp.myfn() },mc.cores=numcores)
+mysim2.arr<-simplify2array(mysim2)
+apply(mysim2.arr,1,mean,na.rm=TRUE) ## proportion of the time that qca and isis/scad and lasso identified the right terms
+apply(mysim2.arr,1,function(x){mean(is.na(x)) }) ## what proportion NAs in each method?
+
+save(mysim2,file="mysim2.rda")
 
